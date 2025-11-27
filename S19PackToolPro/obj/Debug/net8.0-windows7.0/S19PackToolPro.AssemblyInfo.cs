2e45dc27865f8d83a7d890238a61a721951c40af
彭经伟 <jwpeng@shinry.com>
2025-11-27 11:03:28 +0800

    更新CRC和HASH算法DLL为x64

--- S19PackToolPro/obj/Debug/net8.0-windows7.0/S19PackToolPro.AssemblyInfo.cs
+++ S19PackToolPro/obj/Debug/net8.0-windows7.0/S19PackToolPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ceb520d7132e21d1bc23776d8b5100db33aea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905bef1d9a64a95dbbf2512f57ac463ee750737f")]
 [assembly: System.Reflection.AssemblyProductAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- S19PackToolPro/obj/Release/net8.0-windows7.0/S19PackToolPro.AssemblyInfo.cs
+++ S19PackToolPro/obj/Release/net8.0-windows7.0/S19PackToolPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9bcf80a37129ea3421a545c9eaf47047d867c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905bef1d9a64a95dbbf2512f57ac463ee750737f")]
 [assembly: System.Reflection.AssemblyProductAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S19PackToolPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
